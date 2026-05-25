5ac35d7af1662191ea6678244240c117474ac76e
crrrrrrrrrrrr <cynthiarong2012@gmail.com>
2026-05-25 16:52:58 -0400

    put everything back in 1 script for now

--- python-script/RunSegmentation.sh
+++ python-script/RunSegmentation.sh
@@ -4,6 +4,3 @@
 /Applications/Imaging/Slicer.app/Contents/MacOS/Slicer \
 --python-script InitialSegmentation.py
 
-echo "Finished initial segmentation, now running myocardium segmentation script"
-/Applications/Imaging/Slicer.app/Contents/MacOS/Slicer \
---python-script MyocardiumSegmentation.py
