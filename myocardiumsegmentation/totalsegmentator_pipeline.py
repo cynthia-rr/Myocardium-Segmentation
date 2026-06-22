848d7b71b7f4434f0349dd8bcd98b5687eddd070
cynthia <98976307+cynthia-rr@users.noreply.github.com>
2026-06-22 11:09:04 -0400

    differences in files imported

--- myocardiumsegmentation/totalsegmentator_pipeline.py
+++ myocardiumsegmentation/totalsegmentator_pipeline.py
@@ -31,14 +31,15 @@ def run_totalsegmentator_pipeline(volume_node: slicer.vtkMRMLScalarVolumeNode) -
     # Create the segmentation nodes
     segmentation_chambers_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_CHAMBERS_NODE_NAME)
     segmentation_effusion_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_EFFUSION_NODE_NAME)
-    segmentation_artery_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_ARTERY_NODE_NAME)
-    segmentation_tissue_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_TISSUE_NODE_NAME)
+    # segmentation_artery_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_ARTERY_NODE_NAME)
+    # segmentation_tissue_node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", SEGMENTATION_TISSUE_NODE_NAME)
 
     # Add segmentation nodes to the dictionary
     name_to_node = {SEGMENTATION_CHAMBERS_NODE_NAME: segmentation_chambers_node, 
                     SEGMENTATION_EFFUSION_NODE_NAME: segmentation_effusion_node, 
-                    SEGMENTATION_ARTERY_NODE_NAME: segmentation_artery_node, 
-                    SEGMENTATION_TISSUE_NODE_NAME: segmentation_tissue_node}
+                    # SEGMENTATION_ARTERY_NODE_NAME: segmentation_artery_node, 
+                    # SEGMENTATION_TISSUE_NODE_NAME: segmentation_tissue_node
+                    }
     
 
     # Define pipeline steps
@@ -55,8 +56,9 @@ def run_totalsegmentator_pipeline(volume_node: slicer.vtkMRMLScalarVolumeNode) -
     # Save outputs
     outputs = [(segmentation_chambers_node, SEGMENTATION_CHAMBERS_FILENAME),
                (segmentation_effusion_node, SEGMENTATION_EFFUSION_FILENAME),
-               (segmentation_artery_node, SEGMENTATION_ARTERY_FILENAME),
-               (segmentation_tissue_node, SEGMENTATION_TISSUE_FILENAME)]
+            #    (segmentation_artery_node, SEGMENTATION_ARTERY_FILENAME),
+            #    (segmentation_tissue_node, SEGMENTATION_TISSUE_FILENAME)
+            ]
 
     for node, filename in outputs:
         slicer.util.saveNode(node, str(PATH_FOR_SAVE/filename))
